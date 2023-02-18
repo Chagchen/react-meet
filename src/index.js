fc6afb29eb814b57cf7a6c5cfc7df8965726cb17
chagchen <chagchen@outlook.com>
2023-02-17 16:27:24 -0800

    inital setup, react-route v6, pages created

--- src/index.js
+++ src/index.js
@@ -1,17 +1,15 @@
-import React from 'react';
+//setup for React 18 
 import ReactDOM from 'react-dom/client';
+import React from 'react';
 import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import App from './App.js';
+import { BrowserRouter } from 'react-router-dom';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+//create rootElement and get root
+const rootElement = document.getElementById('root');
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+// create a root
+const root = ReactDOM.createRoot(rootElement);
+
+//render app to root
+root.render(<BrowserRouter><App /></BrowserRouter>);
