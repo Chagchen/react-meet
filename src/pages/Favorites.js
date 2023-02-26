d93fcc8255519e0540b733df649ac2b408e1b949
chagchen <chagchen@outlook.com>
2023-02-25 20:12:41 -0800

    add badge counter to favorites main nav

--- src/pages/Favorites.js
+++ src/pages/Favorites.js
@@ -1,17 +1,26 @@
-import {useContext} from 'react';
-import FavoritesContext from '../store/favorites-context';
+import { useContext } from "react";
+import FavoritesContext from "../store/favorites-context";
 import MeetupList from "../components/meetups/MeetupList";
 import { jsx as _jsx } from "react/jsx-runtime";
 import React from "react";
-
-
+//only temp memory used on page re-load all fav content disapears
 function FavoritesPage() {
   const favoritesCtx = useContext(FavoritesContext);
 
-  return <section>
-    <h1>My Favorites</h1>
-  <MeetupList meetups={favoritesCtx.favorites}/>
-  </section>;
+  let content;
+
+  if (favoritesCtx.totalFavorites === 0) {
+    content = <h3> You've got no favorites yet. Start adding some!  </h3>;
+  } else {
+    content = <MeetupList meetups={favoritesCtx.favorites} />;
+  }
+
+  return (
+    <section>
+      <h1 class='font-family: roboto;'>My Favorites</h1>
+      {content}
+    </section>
+  );
 }
 
 export default FavoritesPage;
